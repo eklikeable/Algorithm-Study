ab3a7fa54ee3fa4f22c48f6d996c272f3e6c5b86
ekkim <eklikeable@kakao.com>
2022-10-14 11:59:52 +0900

    청길이의 패션대회

--- 100problems/98.js
+++ 100problems/98.js
@@ -1,10 +1,17 @@
+/**
+ * 문제 98 : 청길이의 패션대회
+ * @param {string} notes - 청길이의 기록
+ * @return {array} numbers - 중복을 제거한 유니크한 옷의 종류
+ */
+
 function uniqueClothes(notes) {
     const numbers = [];
     const memo = notes.split(' ');
     for (let e of memo) {
         if (memo.indexOf(e) % 2 === 1) {
             for (let i of e) {
-                if (parseInt(i) * 1 === parseInt(i) && !numbers.includes(i)) {
+                i = parseInt(i);
+                if (i * 1 === i && !numbers.includes(i)) {
                     numbers.push(i);
                 }
             }
