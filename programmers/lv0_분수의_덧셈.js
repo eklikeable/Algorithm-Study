520e3ae01a704bde1503b5dd3cf12e55c109a204
ekkim <eklikeable@kakao.com>
2022-10-24 22:40:15 +0900

    getGCD 수정

--- programmers/lv0_분수의_덧셈.js
+++ programmers/lv0_분수의_덧셈.js
@@ -1,13 +1,11 @@
 function solution(denum1, num1, denum2, num2) {
-    const answer = [0, 0];
     const numerator = num1 * denum2 + num2 * denum1;
     const denominator = num1 * num2;
-    const getGCD = (a, b) => (a % b ? getGCD(b, a % b) : b);
+    const getGCD = (a, b) => (b ? getGCD(b, a % b) : a);
     const gcd = getGCD(numerator, denominator);
-    answer[0] = numerator / gcd;
-    answer[1] = denominator / gcd;
-    return answer;
+    return [numerator / gcd, denominator / gcd];
 }
+
 console.log(solution(1.5, 2, 3.5, 4)); // [13, 8]
 console.log(solution(1, 2, 3, 4)); // [5, 4]
 console.log(solution(9, 2, 1, 3)); // [29, 6]
@@ -16,4 +14,4 @@ console.log(solution(9, 2, 1, 3)); // [29, 6]
 // const getGCD = (a, b) => (a % b === 0 ? b : getGCD(b, a % b));
 
 // 최소공배수(LCM)
-// const lcm = (a, b) => a * b / gcd(a, b)
+// const getLCM = (a, b) => a * b / gcd(a, b)
